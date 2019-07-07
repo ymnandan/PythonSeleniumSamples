2f6ce3d0bbeff9dfd85c2e7912b1ca9348b17db3
ymnandan <ymnandan.gmail.com>
2019-07-07 11:22:43 +0530

    Find Element example program added

---
+++ FindElementExample.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+
+driver = webdriver.Firefox(executable_path="/home/nandan/WebDrivers/geckodriver")
+
+driver.get("http://www.google.com")
+
+element=driver.find_element_by_name("q")
+
+element.send_keys("hello world")
