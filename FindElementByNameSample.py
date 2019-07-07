efe1f9ab75d28c430479fbf34a8bc90d44aa77d3
suhas <suhas.rnr@gmail.com>
2019-07-07 15:41:59 +0530

    Second commit

---
+++ FindElementByNameSample.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+
+driver = webdriver.Firefox(executable_path="/home/nandan/WebDrivers/geckodriver")
+
+driver.get("http://www.google.com")
+
+element=driver.find_element_by_name("q")
+
+element.send_keys("hello world")
+
+element.submit()
+driver.quit()
